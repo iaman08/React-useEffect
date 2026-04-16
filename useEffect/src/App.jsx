bf4d949b58f1130ffa89ff1c62c23cf22c24e23d
iaman08 <amanraj.cs@yahoo.com>
2026-04-16 10:22:37 +0530

    Initial commit

---
+++ useEffect/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+
+  useEffect(()=>{
+   const time_interval = setInterval(()=>{
+     setCount(prevcount => prevcount + 1);
+    },1000);
+ 
+    return () => {
+      clearInterval(time_interval);
+    };
+   },[]);
+
+  return(
+    <div>
+      <h3>Value is: {count}</h3>
+    </div>
+  )
+}
+
+export default App;
